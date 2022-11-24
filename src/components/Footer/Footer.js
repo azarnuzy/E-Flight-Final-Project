ae3fc61cc6688af3a8f81da2549521d961766b51
azarnuzy <azarnuzy@gmail.com>
2022-11-24 20:23:57 +0700

    add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="w-full flex items-center justify-center flex-col absolute bottom-0 bg-primary py-3">
+      <div className="flex justify-center flex-col text-center text-white">
+        <div className="font-medium">
+          <span className="font-semibold">FlyKet </span> is a E-Flight Company{' '}
+        </div>
+        <span className="font-medium">
+          copyright © 2022, made with ❤️ in Indonesia
+        </span>
+      </div>
+    </div>
+  );
 }
