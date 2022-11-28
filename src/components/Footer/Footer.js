7225cfd9cf39571b3f8133869eacb156054ed065
FadillahNurIstiqomah <fadillahistiqomah@students.amikom.ac.id>
2022-11-28 19:31:04 +0700

    Update Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from '../../assets/Logo.png';
 import { FaYoutube, FaInstagram, FaTwitter, FaFacebook } from 'react-icons/fa';
 
 export default function Footer() {
@@ -14,22 +15,35 @@ export default function Footer() {
     //   </div>
     // </div>
     <footer>
-      <div className="text-center items-center text-white bg-primary p-10">
-        <div className="flex gap-2 justify-center my-5 text-lg">
-          <a href="https://youtube.com/">
-            <FaYoutube className="hover:text-color5" />
-          </a>
-          <a href="https://instagram.com/">
-            <FaInstagram className="hover:text-color5" />
-          </a>
-          <a href="https://facebook.com/">
-            <FaFacebook className="hover:text-color5" />
-          </a>
-          <a href="https://twitter.com/">
-            <FaTwitter className="hover:text-color5" />
-          </a>
+      <div className="text-center items-center text-white bg-primary p-5">
+        <div className='grid grid-cols-3 mt-5'>
+          <div className='items-start mx-5'>
+            <div className='flex flex-row gap-2 my-2'>
+              <img src={logo} alt="" className="w-[30px]" />{' '}
+              <span className="text-white text-2xl">FlyKet</span>
+            </div>
+            <p className='text-white text-justify w-[300px]'>Flyket is E-flight Website </p>
+          </div>
+          <div className='flex flex-col gap-2 text-white m-5'>
+            <p>Privacy Police</p>
+            <p>About Us</p>
+            <p>Terms of Use</p>
+          </div>
+          <div className="flex gap-2 justify-center items-center my-5 text-xl">
+            <a href="https://youtube.com/">
+              <FaYoutube className="hover:text-color5" />
+            </a>
+            <a href="https://instagram.com/">
+              <FaInstagram className="hover:text-color5" />
+            </a>
+            <a href="https://facebook.com/">
+              <FaFacebook className="hover:text-color5" />
+            </a>
+            <a href="https://twitter.com/">
+              <FaTwitter className="hover:text-color5" />
+            </a>
+          </div>
         </div>
-        <p>Explore the world with Flyket.</p>
         <p className="mt-2 text-white">
           Copyright &copy; 2022 Flyket. All Right Reserved.
         </p>
