b9d33cba1237b75ab8a6f199f09cd2ccbd0100c6
azarnuzy <azarnuzy@gmail.com>
2022-11-28 10:52:01 +0700

    modified something

---
+++ src/api/apiConfig.js
@@ -0,0 +1,12 @@
+// const apiConfig = {
+//     baseUrl: 'https://api.themoviedb.org/3/',
+//     userUrl: 'https://notflixtv.herokuapp.com/api/v1/users/',
+//     apiKey: '723efb5ac2ef3c9278d3e18eb6184aa3',
+//     clientId:
+//       '907788144298-3e4o1rkr1jpq5k9lduqs417ij2r061jk.apps.googleusercontent.com',
+//     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+//     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
+//   };
+  
+//   export default apiConfig;
+  
