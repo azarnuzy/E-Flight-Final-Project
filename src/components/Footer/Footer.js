609c1d32d8a50f5eca9d00ebbe3c4d6a2572d16d
FadillahNurIstiqomah <fadillahistiqomah@students.amikom.ac.id>
2022-11-30 12:25:20 +0700

    Newest Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -16,35 +16,38 @@ export default function Footer() {
     // </div>
     <footer>
       <div className="text-center items-center text-white bg-primary p-5">
-        <div className='grid grid-cols-3 mt-5'>
-          <div className='items-start mx-5'>
-            <div className='flex flex-row gap-2 my-2'>
-              <img src={logo} alt="" className="w-[30px]" />{' '}
-              <span className="text-white text-2xl">FlyKet</span>
+        <div className='grid grid-cols-3 mt-10'>
+          <div className='items-start ml-[60px]'>
+            <div className='flex flex-row gap-2 my-2 mx-auto'>
+              <img src={logo} alt="" className="w-[25px]" />
+              <span className="text-white text-xl">FlyKet</span>
             </div>
-            <p className='text-white text-justify w-[300px]'>Flyket is E-flight Website </p>
+            <p className='text-white text-justify text-sm w-[350px]'>Flyket is an electronic plane ticket purchasing website that makes it easier and more effective</p>
           </div>
-          <div className='flex flex-col gap-2 text-white m-5'>
-            <p>Privacy Police</p>
-            <p>About Us</p>
-            <p>Terms of Use</p>
+          <div className='flex flex-col gap-1 text-white m-3 ml-10'>
+            <button className='bg-primary hover:text-color5'><p>Contact Us</p></button>
+            <button className='bg-primary hover:text-color5'><p>About Us</p></button>
+            <button className='bg-primary hover:text-color5'><p>Terms of Use</p></button>
           </div>
-          <div className="flex gap-2 justify-center items-center my-5 text-xl">
-            <a href="https://youtube.com/">
-              <FaYoutube className="hover:text-color5" />
-            </a>
-            <a href="https://instagram.com/">
-              <FaInstagram className="hover:text-color5" />
-            </a>
-            <a href="https://facebook.com/">
-              <FaFacebook className="hover:text-color5" />
-            </a>
-            <a href="https://twitter.com/">
-              <FaTwitter className="hover:text-color5" />
-            </a>
+          <div className='ml-[200px] mt-3'>
+            <h5 className='text-xl font-bold'>Ikuti Kami:</h5>
+            <div className="flex gap-2 justify-center items-center my-5 text-xl">
+              <a href="https://youtube.com/">
+                <FaYoutube className="hover:text-color5" />
+              </a>
+              <a href="https://instagram.com/">
+                <FaInstagram className="hover:text-color5" />
+              </a>
+              <a href="https://facebook.com/">
+                <FaFacebook className="hover:text-color5" />
+              </a>
+              <a href="https://twitter.com/">
+                <FaTwitter className="hover:text-color5" />
+              </a>
+            </div>
           </div>
         </div>
-        <p className="mt-2 text-white">
+        <p className="mt-10 text-white">
           Copyright &copy; 2022 Flyket. All Right Reserved.
         </p>
       </div>
