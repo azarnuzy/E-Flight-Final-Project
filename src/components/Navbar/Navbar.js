18cbc605e4c122f8891bb7ba098aab0e989ee115
FadillahNurIstiqomah <fadillahistiqomah@students.amikom.ac.id>
2022-12-01 19:59:23 +0700

    Add state and profile picture

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,6 +6,7 @@ import { IoIosNotifications } from 'react-icons/io';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { getLogin } from '../../features/user/userSlice';
+import ava from '../../assets/profile_picture.png'
 
 export default function Navbar() {
   const navigate = useNavigate();
@@ -18,45 +19,23 @@ export default function Navbar() {
         <img src={logo} alt="" className="w-[40px]" />{' '}
         <span className="text-white text-2xl">FlyKet</span>
       </Link>
-      {/* <div className="py-2 border-t-0 border-x-0 border-b-2 border-slate-50 w-[40vw] border-solid  my-3 lg:flex justify-between items-center hidden">
-        <input
-          type="text"
-          className="outline-none bg-transparent placeholder:text-gray-200 ml-3 w-full"
-          id="search-movie"
-          placeholder="Search"
-          // value={keyword}
-          // onChange={(e) => setKeyword(e.target.value)}
-          // onKeyDown={(e) => {
-          //   if (keyword.length > 0) {
-          //     handleKeyPressed(e);
-          //   }
-          // }}
-        />
-        <label htmlFor="search-movie">
-          <AiOutlineSearch className="text-white mr-3" />
-        </label>
-      </div> */}
       <div className="flex gap-3 items-center">
         <button onClick={() => navigate(`/notification`)}>
           <IoIosNotifications className="text-white text-2xl" />
         </button>
         {isLogin ? 
-          <div>
-            <p>Ini sudah login</p>
-          </div>
+          <Link to={'/myprofile'}>
+            <img src={ava} alt='' className='w-8'/>
+          </Link>
         :
           
           <Link
             className="text-primary bg-white py-1 px-3 rounded-md"
-            // onClick={() => navigate(`/login`)}n
             to={'/login'}
           >
             Login
           </Link>
         }
-        <Link className="bg-primary text-white py-1 px-3" to={'/myprofile'}>
-          My Profile
-        </Link>
       </div>
     </div>
   );
