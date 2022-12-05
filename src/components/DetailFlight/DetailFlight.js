a0a1538a050988cab74fcdf6dbc334ad6f848560
Wiridho Partuaon Tambunan <wiridho@student.telkomuniversity.ac.id>
2022-12-05 20:48:52 +0700

    Detail Flight Fix

--- src/components/DetailFlight/DetailFlight.js
+++ src/components/DetailFlight/DetailFlight.js
@@ -1,19 +1,27 @@
 import React from 'react'
-import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
+import Navbar from '../Navbar/Navbar'
+import DetailPemesanan from './DetailPemesanan'
+import DetailPenumpang from './DetailPenumpang'
+import TotalFlight from './TotalFlight'
+// import Footer from '../Footer/Footer'
+
+
 
 export default function DetailFlight() {
 	return (
 		<div>
 			<Navbar />
-			<div className='md:flex md:max-w-[1024px] mx-auto md:mt-24 md:justify-between'>
-				<div>
-					<h1>Detail Pemesanan</h1>
+			<div className='flex gap-3 md:max-w-[1024px] mx-auto md:mt-24 '>
+				<div className='md:w-2/3'>
+					<DetailPemesanan />
+					<DetailPenumpang />
 				</div>
-
-
+				<div className='md:w-1/3' >
+					<TotalFlight />
+				</div>
 			</div>
-			{/* <Footer /> */}
-		</div>
+			<Footer />
+		</div >
 	)
 }
