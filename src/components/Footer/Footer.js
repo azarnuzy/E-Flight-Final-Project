e8c773736b5941d5edc37ba23c5a1b3c12d05e0b
azarnuzy <azarnuzy@gmail.com>
2022-12-09 21:28:50 +0700

    fix responsive footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -16,21 +16,30 @@ export default function Footer() {
     // </div>
     <footer>
       <div className="text-center items-center text-white bg-primary p-5">
-        <div className='grid lg:grid-cols-3 grid-cols-1 mt-10'>
-          <div className='items-start ml-[60px]'>
-            <div className='flex flex-row gap-2 my-2 mx-auto'>
+        <div className="grid lg:grid-cols-3 grid-cols-1 mt-10">
+          <div className="lg:ml-[60px] mx-auto">
+            <div className="flex flex-row gap-2 my-2">
               <img src={logo} alt="" className="w-[25px]" />
               <span className="text-white text-xl">FlyKet</span>
             </div>
-            <p className='text-white text-justify text-sm w-[350px]'>Flyket is an electronic plane ticket purchasing website that makes it easier and more effective</p>
+            <p className="text-white text-justify text-sm lg:w-[350px] w-[300px]">
+              Flyket is an electronic plane ticket purchasing website that makes
+              it easier and more effective
+            </p>
           </div>
-          <div className='flex flex-col gap-1 text-white m-3 ml-10'>
-            <button className='bg-primary hover:text-color5'><p>Contact Us</p></button>
-            <button className='bg-primary hover:text-color5'><p>About Us</p></button>
-            <button className='bg-primary hover:text-color5'><p>Terms of Use</p></button>
+          <div className="flex flex-col gap-1 text-white lg:m-3 lg:ml-10 mt-8">
+            <button className="bg-primary hover:text-color5">
+              <p>Contact Us</p>
+            </button>
+            <button className="bg-primary hover:text-color5">
+              <p>About Us</p>
+            </button>
+            <button className="bg-primary hover:text-color5">
+              <p>Terms of Use</p>
+            </button>
           </div>
-          <div className='lg:ml-[200px] mt-3'>
-            <h5 className='text-xl font-bold'>Ikuti Kami:</h5>
+          <div className="lg:ml-[200px] lg:mt-3 mt-6">
+            <h5 className="text-xl font-bold">Ikuti Kami:</h5>
             <div className="flex gap-2 justify-center items-center my-5 text-xl">
               <a href="https://youtube.com/">
                 <FaYoutube className="hover:text-color5" />
@@ -47,10 +56,11 @@ export default function Footer() {
             </div>
           </div>
         </div>
-        <p className="mt-10 text-white">
+        <p className="mt-10 mb-5 lg:mb-0 text-white">
           Copyright &copy; 2022 Flyket. All Right Reserved.
         </p>
       </div>
+          
     </footer>
   );
 }
