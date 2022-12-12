5620aeb0d49ac28e91c4aac04f4a2a3cedcec178
azarnuzy <azarnuzy@gmail.com>
2022-12-12 19:18:56 +0700

    there are error when update user

--- src/components/UserProfile/AccountInformation.js
+++ src/components/UserProfile/AccountInformation.js
@@ -1,18 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchUser, getUser } from '../../features/user/userSlice';
+import Swal from 'sweetalert2';
+import { editUser, fetchUser, getUser } from '../../features/user/userSlice';
 // Import Components
 import ListTitleCategory from './ListTitleCategory';
 export default function AccountInformation() {
+  const user = useSelector(getUser);
+  const dispatch = useDispatch();
+
   let email = JSON.parse(localStorage.getItem('email'));
   const [isEdit, setIsEdit] = useState(false);
 
+  const [firstName, setFirstName] = useState(user.firstName);
+  const [lastName, setLastName] = useState(user.lastName);
+  const [phoneNumber, setPhoneNumber] = useState(user.phoneNumber);
+
   const handleEdit = async () => {
-    setIsEdit((isEdit) => !isEdit);
+    try {
+      setIsEdit((isEdit) => !isEdit);
+    } catch (error) {}
   };
 
-  const user = useSelector(getUser);
-  const dispatch = useDispatch();
+  const handleEditAccount = (email, firstName, lastName, phoneNumber) => {
+    console.log(email, firstName, lastName, phoneNumber);
+    try {
+      dispatch(editUser({ email, firstName, lastName, phoneNumber }));
+    } catch (error) {}
+  };
 
   useEffect(() => {
     dispatch(fetchUser(email));
@@ -25,7 +39,11 @@ export default function AccountInformation() {
             <h1 className="font-medium">Account</h1>
             <button
               className="text-primary hover:text-thirdly"
-              onClick={handleEdit}
+              onClick={() => {
+                // console.log(email, firstName, lastName, phoneNumber);
+                handleEdit();
+                // handleEditAccount(email, firstName, lastName, phoneNumber);
+              }}
             >
               Save
             </button>
@@ -33,26 +51,42 @@ export default function AccountInformation() {
           <div className="px-4">
             <div className="w-full md:w-1/3">
               <p className="text-base text-userProfile">Title</p>
-              <ListTitleCategory />
+              <p>{user.title}</p>
+              {/* <ListTitleCategory /> */}
             </div>
             <div className="md:flex-row flex flex-col mb-2 mt-4">
               <div className="mr-4">
                 <p className="text-base text-userProfile ">First Name</p>
-                <input className='mt-1 px-3 py-1 bg-white border shadow-sm border-slate-300 placeholder-slate-400  focus:outline-none focus:border-primary focus:primary block w-full rounded-md sm:text-sm focus:ring-1" placeholder="you@example.com"' />
+                <input
+                  className="mt-1 px-3 py-1 bg-white border shadow-sm border-slate-300 placeholder-slate-400  focus:outline-none focus:border-primary focus:primary block w-full rounded-md sm:text-sm focus:ring-1"
+                  placeholder="First Name"
+                  onChange={(e) => setFirstName(e.target.value)}
+                  value={firstName}
+                />
               </div>
               <div>
                 <p className="text-base text-userProfile">Last Name</p>
-                <input className='mt-1 px-3 py-1 bg-white border shadow-sm border-slate-300 placeholder-slate-400  focus:outline-none focus:border-primary focus:primary block w-full rounded-md sm:text-sm focus:ring-1" placeholder="you@example.com"' />
+                <input
+                  className="mt-1 px-3 py-1 bg-white border shadow-sm border-slate-300 placeholder-slate-400  focus:outline-none focus:border-primary focus:primary block w-full rounded-md sm:text-sm focus:ring-1"
+                  placeholder="Last Name"
+                  onChange={(e) => setLastName(e.target.value)}
+                  value={lastName}
+                />
               </div>
             </div>
             <div className="mb-4">
               <div className="my-2">
                 <p className="text-base text-userProfile">Email</p>
-                <span className="text-sm">marcus.holloway@gmail.com</span>
+                <span className="text-sm">{email}</span>
               </div>
               <div className="">
                 <p className="text-base text-userProfile">Phone Number</p>
-                <input className='mt-1 md:w-1/2 px-3 py-1 bg-white border shadow-sm border-slate-300 placeholder-slate-400  focus:outline-none focus:border-primary focus:primary block w-full rounded-md sm:text-sm focus:ring-1" placeholder="you@example.com"' />
+                <input
+                  className="mt-1 md:w-1/2 px-3 py-1 bg-white border shadow-sm border-slate-300 placeholder-slate-400  focus:outline-none focus:border-primary focus:primary block w-full rounded-md sm:text-sm focus:ring-1 "
+                  placeholder="Phone Number"
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                  value={phoneNumber}
+                />
               </div>
             </div>
           </div>
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -21,6 +21,29 @@ export const fetchUser = createAsyncThunk('user/account', async (email) => {
   } catch (error) {}
 });
 
+export const editUser = createAsyncThunk(
+  'user/editAccount',
+  async ({ email, firstName, lastName, phoneNumber }) => {
+    console.log(email, firstName, lastName, phoneNumber);
+    try {
+      const response = await axiosClient.post(
+        `${apiConfig.baseUrl}users/update`,
+        {
+          params: { email: email },
+        },
+        {
+          firstName: firstName,
+          lastName: lastName,
+          phoneNumber: phoneNumber,
+        }
+      );
+
+      console.log(response.data);
+      return response.data;
+    } catch (error) {}
+  }
+);
+
 export const userSlice = createSlice({
   name: 'user',
   initialState,
@@ -32,10 +55,15 @@ export const userSlice = createSlice({
   extraReducers(builder) {
     builder
       .addCase(fetchUser.fulfilled, (state, action) => {
-        console.log(action.payload);
         state.users = action.payload;
       })
       .addCase(fetchUser.rejected, (state, action) => {
+        console.log(action.error.message);
+      })
+      .addCase(editUser.fulfilled, (state, action) => {
+        state.users = action.payload;
+      })
+      .addCase(editUser.rejected, (state, action) => {
         console.log(action.error.message);
       });
   },
