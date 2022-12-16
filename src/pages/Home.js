7f895f414a99d0eb828815ed8d88b15a3ec51ab7
FadillahNurIstiqomah <fadillahistiqomah@students.amikom.ac.id>
2022-12-16 19:53:03 +0700

    Clear data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Footer from '../components/Footer/Footer';
-import { ContentHomepage } from '../components/Home/ContentHomepage';
 import FormFlight from '../components/Home/FormFlight';
 import Header from '../components/Home/Header';
 import Navbar from '../components/Navbar/Navbar';
@@ -11,7 +10,6 @@ export default function Home() {
       <Navbar />
       <Header />
       <FormFlight />
-      <ContentHomepage/>
       <Footer />
     </div>
   );
