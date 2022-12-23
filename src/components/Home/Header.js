1664dccb977f399398345f95ef9833b62c2af709
FadillahNurIstiqomah <fadillahistiqomah@students.amikom.ac.id>
2022-12-23 20:23:50 +0700

    Update Header

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
-// import background from '../../assets/home-swiper-1.png';
-import background from '../../assets/header.jpg';
+import background from '../../assets/home-swiper-1.png';
+// import background from '../../assets/header.jpg';
 // import background from '../../assets/header1.jpg';
 
 export default function Header() {
