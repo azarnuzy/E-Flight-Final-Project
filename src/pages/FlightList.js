ed7cd07a3f73587ac35844faf5714cda56d173a8
FadillahNurIstiqomah <fadillahistiqomah@students.amikom.ac.id>
2022-12-26 21:26:49 +0700

    Add replacer image

--- src/pages/FlightList.js
+++ src/pages/FlightList.js
@@ -6,21 +6,36 @@ import UserFlight from '../components/FlightList/UserFlight';
 import Footer from '../components/Footer/Footer';
 import Navbar from '../components/Navbar/Navbar';
 import { getSchedules } from '../features/search/searchSlice';
+import replacer from '../assets/no-results.png'
 
 export default function FlightList() {
   const schedules = useSelector(getSchedules);
   // console.log(schedules);
+  const checkSchedules = () => {
+    if (schedules.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
   return (
     <div>
       <Navbar />
       <div className="max-w-[1280px] w-full mx-auto flex">
         <div className="flex lg:flex-row flex-col md:mx-10 w-full gap-3 justify-center">
           <UserFlight />
-          <div className="w-full">
+          <div className="w-full mb-[115px]">
             <FlightSearch />
-            {schedules.map((item, i) => {
-              return <CardFlight key={i} item={item} />;
-            })}
+            {checkSchedules() ? (
+              schedules.map((item, i) => {
+                return <CardFlight key={i} item={item} />;
+              })
+            ) : (
+              <div>
+                <img src={replacer} alt='' className='mx-auto w-[250px] mt-20' />
+              </div>
+              
+            )}
           </div>
         </div>
       </div>
