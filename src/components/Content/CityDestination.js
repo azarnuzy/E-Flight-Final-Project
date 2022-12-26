159d4ae9500509b9d129cf87bfe8cd648044945f
FadillahNurIstiqomah <fadillahistiqomah@students.amikom.ac.id>
2022-12-26 16:33:48 +0700

    Responsive for homepage

--- src/components/Content/CityDestination.js
+++ src/components/Content/CityDestination.js
@@ -12,25 +12,26 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper"
-import { Dialog, Transition } from '@headlessui/react'
+import { useWindowWidth } from '@react-hook/window-size';
 import '../../index.css'
 
 export default function CityDestination (){
-    let [isOpen, setIsOpen] = useState(true)
 
-    function closeModal() {
-        setIsOpen(false)
-    }
+    const width = useWindowWidth();
 
-    function openModal() {
-        setIsOpen(true)
-    }
+    const getSlidesPerView = () => {
+        if (width >= 768) {
+          return 3;
+        } else {
+          return 1;
+        }
+      };
     return(
-        <div className='p-10'>
+        <div className='md:p-10 p-5'>
             <h3 className='font-bold text-3xl text-center'>Popular Cities in Indonesia</h3>
-            <div className='p-10'>
+            <div className='lg:p-10 mt-3'>
                 <Swiper
-                    slidesPerView={3}
+                    slidesPerView={getSlidesPerView()}
                     spaceBetween={30}
                     slidesPerGroup={3}
                     loop={true}
@@ -39,56 +40,56 @@ export default function CityDestination (){
                     modules={[Navigation]}
                     className="mySwiper"
                 >
-                    <SwiperSlide onClick={openModal} className='cursor-pointer'>
-                        <img src={city1} alt='' className='brightness-50 z-1'></img>
+                    <SwiperSlide className='cursor-pointer'>
+                        <img src={city1} alt='' className='brightness-50 z-1 w-full'></img>
                         <div className='text-city w-full'>
-                            <p className='md:text-2xl font-semibold text-center'>Yogyakarta</p>
+                            <p className='md:text-2xl text-xl font-semibold text-center'>Yogyakarta</p>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img src={city2} alt='' className='brightness-50 z-1'></img>
+                        <img src={city2} alt='' className='brightness-50 z-1 w-full'></img>
                         <div className='text-city w-full'>
-                            <p className='md:text-2xl font-semibold text-center'>Bali</p>
+                            <p className='md:text-2xl text-xl font-semibold text-center'>Bali</p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide> 
+                        <img src={city3} alt='' className='brightness-50 z-1 w-full'></img>
+                        <div className='text-city w-full'>
+                            <p className='md:text-2xl text-xl font-semibold text-center'>Jakarta</p>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img src={city3} alt='' className='brightness-50 z-1'></img>
+                        <img src={city4} alt='' className='brightness-50 z-1 w-full'></img>
                         <div className='text-city w-full'>
-                            <p className='md:text-2xl font-semibold text-center'>Jakarta</p>
+                            <p className='md:text-2xl text-xl font-semibold text-center'>Surabaya</p>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img src={city4} alt='' className='brightness-50 z-1'></img>
+                        <img src={city5} alt='' className='brightness-50 z-1 w-full'></img>
                         <div className='text-city w-full'>
-                            <p className='md:text-2xl font-semibold text-center'>Surabaya</p>
+                            <p className='md:text-2xl text-xl font-semibold text-center'>Malang</p>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img src={city5} alt='' className='brightness-50 z-1'></img>
+                        <img src={city6} alt='' className='brightness-50 z-1 w-full'></img>
                         <div className='text-city w-full'>
-                            <p className='md:text-2xl font-semibold text-center'>Malang</p>
+                            <p className='md:text-2xl text-xl font-semibold text-center'>Surakarta</p>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img src={city6} alt='' className='brightness-50 z-1'></img>
+                        <img src={city7} alt='' className='brightness-50 z-1 w-full'></img>
                         <div className='text-city w-full'>
-                            <p className='md:text-2xl font-semibold text-center'>Surakarta</p>
+                            <p className='md:text-2xl text-xl font-semibold text-center'>Bandung</p>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img src={city7} alt='' className='brightness-50 z-1'></img>
+                        <img src={city8} alt='' className='brightness-50 z-1 w-full'></img>
                         <div className='text-city w-full'>
-                            <p className='md:text-2xl font-semibold text-center'>Bandung</p>
+                            <p className='md:text-2xl text-xl font-semibold text-center'>Padang</p>
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img src={city8} alt='' className='brightness-50 z-1'></img>
-                        <div className='text-city w-full'>
-                            <p className='md:text-2xl font-semibold text-center'>Padang</p>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <img src={city9} alt='' className='brightness-50 z-1'></img>
+                        <img src={city9} alt='' className='brightness-50 z-1 w-full'></img>
                         <div className='text-city w-full'>
                             <p className='md:text-2xl font-semibold text-center'>Papua</p>
                         </div>
--- src/components/Content/contentHomepage.js
+++ src/components/Content/contentHomepage.js
@@ -11,13 +11,13 @@ export default function Homepage (){
     <div>
       <CityDestination/>
       <div className='bg-color6 p-10'>
-        <div className='flex flex-row md:w-3/4 w-full mx-auto'>
+        <div className='flex md:flex-row flex-col md:w-3/4 md:h-3/4 h-full w-full mx-auto'>
           <div>
-            <img src={content1} alt='' className='w-[500px] h-[360px] py-3 px-1'></img>
+            <img src={content1} alt='' className='py-3 md:px-1'></img>
           </div>
-          <div className='mt-10 ml-5'>
-            <h2 className='text-[30px] font-bold py-3 px-4 ml-5'>Get Flight Tickets to Your Favorite Destinations</h2>
-            <p className='w-[600px] ml-8 text-justify'>Want to go on vacation with your family? Now, you can directly order flight tickets at Flyket. 
+          <div className='mt-10 lg:ml-5'>
+            <h2 className='text-[30px] font-bold py-3 lg:px-4 lg:ml-5 lg:w-fit w-full'>Get Flight Tickets to Your Favorite Destinations</h2>
+            <p className='lg:w-[600px] w-full lg:ml-8 text-justify'>Want to go on vacation with your family? Now, you can directly order flight tickets at Flyket. 
                   Flyket always provides reliable innovations to make it easier for you when ordering flight tickets online. 
                   Search for airline tickets online at Flyket to domestic and international flight destinations around the world. 
                   You can check flight ticket prices online at Flyket, as well as compare flight schedules directly via online.
@@ -27,26 +27,26 @@ export default function Homepage (){
       </div>
       <div className='p-10 px-[100px]'>
         <h2 className='font-bold text-[28px] p-5 mb-5 text-center'>Why Using Flyket?</h2>
-        <div className='flex flex-row justify-between mb-10'>
+        <div className='flex lg:flex-row flex-col justify-between mb-10'>
           <div>
             <img src={pict1} alt='' className='w-[100px] py-5 mx-auto'/>
             <h2 className='font-bold text-xl text-center'>Transaction from Anywhere</h2>
-            <p className='w-[300px] text-center'>Make a transaction from anywhere at any time, from desktop, mobile app, or mobile web.</p>
+            <p className='lg:w-[300px] text-center w-full mt-2'>Make a transaction from anywhere at any time, from desktop, mobile app, or mobile web.</p>
           </div>
           <div>
             <img src={pict2} alt='' className='w-[100px] py-5 mx-auto'/>
             <h2 className='font-bold text-xl text-center'>Get Your Invoice</h2>
-            <p className='w-[300px] text-center'>Get an invoice for payment of your order to avoid misunderstandings in the future.</p>
+            <p className='lg:w-[300px] text-center w-full mt-2'>Get an invoice for payment of your order to avoid misunderstandings in the future.</p>
           </div>
           <div>
             <img src={pict3} alt='' className='w-[100px] py-5 mx-auto'/>
             <h2 className='font-bold text-xl text-center'>Ease of Payment</h2>
-            <p className='w-[300px] text-center'>Payment for transactions is easy because you can use digital wallets and offline payment outlets.</p>
+            <p className='lg:w-[300px] text-center w-full mt-2'>Payment for transactions is easy because you can use digital wallets and offline payment outlets.</p>
           </div>
           <div>
             <img src={pict4} alt='' className='w-[100px] py-5 mx-auto'/>
             <h2 className='font-bold text-xl text-center'>Secure Transaction</h2>
-            <p className='w-[300px] text-center'>The security of data and transactions is guaranteed so you don't have to worry about using Flyket.</p>
+            <p className='lg:w-[300px] text-center w-full mt-2'>The security of data and transactions is guaranteed so you don't have to worry about using Flyket.</p>
           </div>
         </div>
       </div>
